f8362e8d7ccced2cb0ba82c2f0f940b1ef563fe1
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-05-18 14:52:02 +0300

    initial commit

---
+++ src/main/java/com/agan/jdbcapinamedparametersdaolayer/JdbcApiJdbcTemplateNamedParametersHwTask1DaoLayerApplication.java
@@ -0,0 +1,14 @@
+package com.agan.jdbcapinamedparametersdaolayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApiJdbcTemplateNamedParametersHwTask1DaoLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcApiJdbcTemplateNamedParametersHwTask1DaoLayerApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/agan/jdbcapinamedparametersdaolayer/JdbcApiJdbcTemplateNamedParametersHwTask1DaoLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agan.jdbcapinamedparametersdaolayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApiJdbcTemplateNamedParametersHwTask1DaoLayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
