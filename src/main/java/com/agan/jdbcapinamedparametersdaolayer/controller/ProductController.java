518f921f39a0033982e5e96becf81c5f650dacfd
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-05-18 15:27:56 +0300

    productController added

---
+++ src/main/java/com/agan/jdbcapinamedparametersdaolayer/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.agan.jdbcapinamedparametersdaolayer.controller;
+
+import com.agan.jdbcapinamedparametersdaolayer.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+
+    @GetMapping("/fetch-product")
+    public ResponseEntity getProductNamesByName(@RequestParam String firstName) {
+
+        var listProducts = productService.getProductNamesByName(firstName);
+
+        return ResponseEntity.ok(listProducts);
+    }
+}
